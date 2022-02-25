3cf2c62a4cdf13abab084add93a9da8e27592b59
Viktoria <ragimovaviktoria95@gmail.com>
2022-02-25 21:05:20 +0700

    функции переделанные

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-function getRandom (min, max) {
+function getRandomNumber (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (max < min) {
-    return Math.floor(Math.random() * (min - max + 1)) + max;
+  if (max < min || max === min || min <= 0) {
+    return;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandom(1,5));
+getRandomNumber(1,5);
 
-function checkCommentLen (commentLen, maxCommentLen) {
-  if (commentLen > maxCommentLen) {
-    return false;
-  }
-  return true;
+function checkCommentLength (userComment, maxCommentLength) {
+  return (userComment.length <= maxCommentLength);
 }
-if (checkCommentLen(1,5)) {
-  console.log('norm');
-}
+checkCommentLength('qwe', 5);
