2e80ffc497436feb120a554f42e3ab4bce764d5f
Viktoria <ragimovaviktoria95@gmail.com>
2022-03-05 23:38:36 +0700

    second version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomNumber (min, max) {
+function getRandomNaturalNumber (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min || min <= 0) {
@@ -6,15 +6,26 @@ function getRandomNumber (min, max) {
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomNumber(1,5);
+getRandomNaturalNumber(1,5);
+
+function getRandomIntegerPositiveNumber (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max <= min || min < 0) {
+    return;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomIntegerPositiveNumber(0,1);
+
 
 function checkCommentLength (userComment, maxCommentLength) {
   return (userComment.length <= maxCommentLength);
 }
 checkCommentLength('qwe', 5);
 
-const descriptionString = 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое. Лежа на панцирнотвердой спине, он видел, стоило ему приподнять голову, свой коричневый, выпуклый, разделенный дугообразными чешуйками живот, на верхушке которого еле держалось готовое вот-вот окончательно сползти одеяло. Его многочисленные, убого тонкие по сравнению с остальным телом ножки беспомощно копошились у него перед глазами. Его комната, настоящая, разве что слишком маленькая, но обычная комната, мирно покоилась в своих четырех хорошо знакомых стенах. На портрете была изображена дама в меховой шляпе и боа, она сидела очень прямо и протягивала зрителю тяжелую меховую муфту, в которой целиком исчезала ее рука. Затем взгляд Грегора устремился в окно, и пасмурная погода – слышно было, как по жести подоконника стучат капли дождя – привела его и вовсе в грустное настроение. «Хорошо бы еще немного поспать и забыть всю эту чепуху». Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. Я совсем один и блаженствую в здешнем краю, словно созданном для таких, как я. Я так счастлив, мой друг, так упоен ощущением покоя, что искусство мое страдает от этого. Ни одного штриха не мог бы я сделать, а никогда не был таким большим художником, как в эти минуты.';
-const DESCRIPTIONS = descriptionString.split('. ');
+const DESCRIPTIONS_STRING = 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое. Лежа на панцирнотвердой спине, он видел, стоило ему приподнять голову, свой коричневый, выпуклый, разделенный дугообразными чешуйками живот, на верхушке которого еле держалось готовое вот-вот окончательно сползти одеяло. Его многочисленные, убого тонкие по сравнению с остальным телом ножки беспомощно копошились у него перед глазами. Его комната, настоящая, разве что слишком маленькая, но обычная комната, мирно покоилась в своих четырех хорошо знакомых стенах. На портрете была изображена дама в меховой шляпе и боа, она сидела очень прямо и протягивала зрителю тяжелую меховую муфту, в которой целиком исчезала ее рука. Затем взгляд Грегора устремился в окно, и пасмурная погода – слышно было, как по жести подоконника стучат капли дождя – привела его и вовсе в грустное настроение. «Хорошо бы еще немного поспать и забыть всю эту чепуху». Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. Я совсем один и блаженствую в здешнем краю, словно созданном для таких, как я. Я так счастлив, мой друг, так упоен ощущением покоя, что искусство мое страдает от этого. Ни одного штриха не мог бы я сделать, а никогда не был таким большим художником, как в эти минуты.';
+const DESCRIPTIONS =  DESCRIPTIONS_STRING.split('. ');
 
 const MESSAGES = [
   'Всё отлично!',
@@ -36,35 +47,33 @@ const NAMES = [
   'Вашингтон',
 ];
 
-const createComment = function (i) {
-  const newComment = {
-    id: i,
-    avatar: `img/avatar-${  getRandomNumber(1, 6)  }.svg`,
-    message:  MESSAGES[getRandomNumber(1, MESSAGES.length) - 1],
-    name: NAMES[getRandomNumber(1, NAMES.length) - 1],
-  };
-  return newComment;
+const createCommentsList = function() {
+  const commentsList = [];
+  for (let i = 1; i <= getRandomNaturalNumber(1, 15); i++) {
+    const newComment = {
+      id: i,
+      avatar: `img/avatar-${  getRandomNaturalNumber(1, 6)  }.svg`,
+      message:  MESSAGES[getRandomIntegerPositiveNumber(0, MESSAGES.length-1)],
+      name: NAMES[getRandomIntegerPositiveNumber(0, NAMES.length-1)],
+    };
+    commentsList.push(newComment);
+  }
+  return commentsList;
 };
 
-const COMMENTS = [];
-for (let i = 1; i <= getRandomNumber(1, 15); i++) {
-  COMMENTS.push(createComment(i));
-}
-
-const createPhoto = function (i) {
-  const newPhoto = {
+const createPhotoDescription = function (i=1) {
+  const newPhotoDescription = {
     id: i,
     url: `photos/${  i  }.jpg`,
-    description: DESCRIPTIONS[getRandomNumber(1, DESCRIPTIONS.length) - 1],
-    likes: getRandomNumber(15, 200),
-    comments: '',
+    description: DESCRIPTIONS[getRandomIntegerPositiveNumber(0, DESCRIPTIONS.length-1)],
+    likes: getRandomNaturalNumber(15, 200),
+    comments: createCommentsList(),
   };
-  return newPhoto;
+  i += 1;
+  return newPhotoDescription;
 };
-const PHOTOS = [];
-for (let i = 1; i <= 25; i++) {
-  PHOTOS.push(createPhoto(i));
-}
 
-PHOTOS();
+const photosDescriptionList = Array.from({length: 25}, createPhotoDescription);
+
+photosDescriptionList();
 
