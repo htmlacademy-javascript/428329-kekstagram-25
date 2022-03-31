9a34b2f3ad5e1170ab3bda17a3c245c0c7b005d8
Viktoria <ragimovaviktoria95@gmail.com>
2022-03-31 20:11:14 +0700

    5th ver

--- js/photo-editor-form-validate.js
+++ js/photo-editor-form-validate.js
@@ -1,6 +1,12 @@
 
 import {contains} from './util.js';
 
+const MAX_DESCRIPTION_LENGTH = 140;
+const MAX_HASHTAGS_AMOUNT = 5;
+const MAX_HASHTAGS_AMOUNT_TEXT = 'Максимальное количество хэштегов - 5';
+const RECURRING_HASHTAGS_ERROR_TEXT = 'Удалите повторяющиеся хэштеги';
+const CORRECT_HASHTAG_ERROR_TEXT = 'Проверьте правильность написания хэштегов';
+
 const uploadForm = document.querySelector('.img-upload__form');
 const userHashtags = document.querySelector('.text__hashtags');
 const description = uploadForm.querySelector('.text__description');
@@ -14,74 +20,76 @@ const pristine = new Pristine (uploadForm, {
 });
 
 const validateDescription = (value) => {
-  if (value.length > 140) {
+  if (value.length > MAX_DESCRIPTION_LENGTH) {
     submitButton.disabled = true;
     return false;
   }
   submitButton.disabled = false;
   return true;
+};
+
+const checkValue = (value) => {
+  const hashtagsArray = value.split(' ');
+  const dublicateArray = [];
+  let error = false;
+
+  if (hashtagsArray[0] === '') {
+    hashtagsArray.splice(0);
+  }
+
+  if (hashtagsArray.length > MAX_HASHTAGS_AMOUNT) {
+    submitButton.disabled = true;
+    return {
+      isValid: false,
+      errorText: MAX_HASHTAGS_AMOUNT_TEXT,
+    };
+  }
+
+  hashtagsArray.forEach((hashtag) => {
+    if (re.test(hashtag) === false) {
+      error = true;
+    }
+    hashtag.toLowerCase();
+    if (contains(dublicateArray, hashtag) === false) {
+      dublicateArray.push(hashtag);
+    }
+  });
+
+  if (hashtagsArray.length !== dublicateArray.length) {
+    submitButton.disabled = true;
+    return {
+      isValid: false,
+      errorText: RECURRING_HASHTAGS_ERROR_TEXT,
+    };
+  }
+
+  if (error) {
+    submitButton.disabled = true;
+    return {
+      isValid: false,
+      errorText: CORRECT_HASHTAG_ERROR_TEXT,
+    };
+  }
+
+  submitButton.disabled = false;
+
+  return {
+    isValid: true,
+    errorText: '',
+  };
 };
 
 const validateHashtag = (value) => {
-  const hashtagsArray = value.split(' ');
-  const dublicateArray = [];
-  let error = false;
-
-  if (hashtagsArray.length > 5) {
-    submitButton.disabled = true;
-    return false;
+  if (value[value.length-1] === ' ') {
+    value = value.substring(0, value.length-1);
   }
-
-  hashtagsArray.forEach((hashtag) => {
-    if (re.test(hashtag) === false) {
-      error = true;
-    }
-    hashtag.toLowerCase();
-    if (contains(dublicateArray, hashtag) === false) {
-      dublicateArray.push(hashtag);
-    }
-  });
-
-  if (hashtagsArray.length !== dublicateArray.length) {
-    submitButton.disabled = true;
-    return false;
-  }
-
-  if (error) {
-    submitButton.disabled = true;
-    return false;
-  }
-  submitButton.disabled = false;
-  return true;
+  const {isValid} = checkValue(value);
+  return isValid;
 };
 
 const getTextError = (value) => {
-  const hashtagsArray = value.split(' ');
-  const dublicateArray = [];
-  let error = false;
-
-  if (hashtagsArray.length > 5) {
-    return 'Максимальное количество хэштегов - 5';
-  }
-
-  hashtagsArray.forEach((hashtag) => {
-    if (re.test(hashtag) === false) {
-      error = true;
-    }
-
-    hashtag.toLowerCase();
-    if (contains(dublicateArray, hashtag) === false) {
-      dublicateArray.push(hashtag);
-    }
-  });
-
-  if (error) {
-    return 'Проверьте правильность написания хэштегов';
-  }
-
-  if (hashtagsArray.length !== dublicateArray.length) {
-    return 'Удалите повторяющиеся хэштеги';
-  }
+  const {errorText} = checkValue(value);
+  return errorText;
 };
 
 pristine.addValidator(description, validateDescription, 'Длина комментария не должна превышать 140 символов.');
--- js/util.js
+++ js/util.js
@@ -62,8 +62,4 @@ function contains(arr, elem) {
   return false;
 }
 
-<<<<<<< HEAD
 export {contains, addComment, removeAllChildren, getRandomNaturalNumber, getRandomIntegerPositiveNumber, checkCommentLength, getRandomArrayElement, isEscapeKey, isEnterKey};
-=======
-export {contains, addComment, removeAllChildren, getRandomNaturalNumber, getRandomIntegerPositiveNumber, checkCommentLength, getRandomArrayElement, isEscapeKey, isEnterKey};
->>>>>>> f0abae6b5b35752f1cd267e723cf35e90a6b8f61
